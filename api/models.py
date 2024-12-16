b6d0fa9fde890953e929123543195753c6233a87
qx3kk <romanchuk.up@gmail.com>
2024-12-17 01:07:38 +0200

    feat: create models django orm

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_update = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
