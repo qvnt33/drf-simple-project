1e27e0bdb342bd9ff14de0b89585c9689eade545
qx3kk <romanchuk.up@gmail.com>
2024-12-17 01:09:58 +0200

    feat: create views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """ModelViewSet автоматично надає CRUD дії для моделі:
+    GET /articles/ -> list (список статей)
+    POST /articles/ -> create (створити статтю)
+    GET /articles/{id}/ -> retrieve (отримати конкретну статтю)
+    PUT /articles/{id}/ -> update (оновити повністю)
+    PATCH /articles/{id}/ -> partial_update (оновити частково)
+    DELETE /articles/{id}/ -> destroy (видалити статтю)
+
+    permission_classes = [IsAuthenticatedOrReadOnly]:
+    - Неавторизовані користувачі можуть лише переглядати (GET), а не створювати/оновлювати/видаляти.
+    - Авторизовані користувачі можуть виконувати всі дії.
+    """
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
